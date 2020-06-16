bdda6d7332e6443f4672341bce8ddd79991fd6a5
Vijit Kamboj <vijitkamboj92@gmail.com>
2020-06-16 14:40:31 +0530

    initial

---
+++ vijitkamboj.github.io/build/precache-manifest.dd7a3fab80de97e57b9ae47f666e66fb.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de2603ae5fbf79e17456a33cf6fcd37e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b9315002e118823b679",
+    "url": "/static/css/2.7df41cd5.chunk.css"
+  },
+  {
+    "revision": "917d1f127f2d421467dd",
+    "url": "/static/css/main.461be830.chunk.css"
+  },
+  {
+    "revision": "0b9315002e118823b679",
+    "url": "/static/js/2.45e66840.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.45e66840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "917d1f127f2d421467dd",
+    "url": "/static/js/main.2e63580c.chunk.js"
+  },
+  {
+    "revision": "8c54849e7c38334dc687",
+    "url": "/static/js/runtime-main.11f167ca.js"
+  },
+  {
+    "revision": "239a3a64b4b90b6dca6ec8a08671a8c1",
+    "url": "/static/media/1920x1080-medium-sky-blue-solid-color-background.239a3a64.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "83b159e8eba4f9d72864488366c078a0",
+    "url": "/static/media/kisspng-computer-icons-iphone-fruit-strawberry-cartoon-5ad8293acb52c0.5001597815241157708328.83b159e8.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
